aede21ce95d799fddf20c0b9421c14250ff7dfa9
shammu1 <shammujob@gmail.com>
2023-09-27 10:12:10 -0700

    Revert "first commit"
    This reverts commit 801914a4aa4cf1467fbb78a0840f69fba6111346.

--- sa_ecomm/core/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms 
-from .models import ProductReview
-
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = ProductReview
-        fields = ['review','rating']
-    
